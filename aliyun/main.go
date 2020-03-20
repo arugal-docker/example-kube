98ff644e61f2bc8d442cbb517d1631550f67e1a4
zhangwei <sunnuslalala@gmail.com>
2020-03-21 01:28:04 +0800

    aliyun trigger

--- aliyun/main.go
+++ aliyun/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"github.com/arugal-docker/example-kube/logger"
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
+	"io/ioutil"
 )
 
 var log *logrus.Logger
@@ -20,19 +20,15 @@ func main() {
 	flag.Parse()
 
 	r := gin.Default()
-	r.POST("/payload", func(c *gin.Context) {
+	r.POST("/trigger", func(c *gin.Context) {
 		if c.Request.Body != nil {
 			defer c.Request.Body.Close()
-			var body []byte
-			_, err := c.Request.Body.Read(body)
-			log.Infof("%s", string(body))
-			if err == nil {
-				trigger := Trigger{}
-				err = json.Unmarshal(body, trigger)
-				if err == nil {
-					log.Infof("%v", trigger)
-				}
+			body, err := ioutil.ReadAll(c.Request.Body)
+			if err != nil {
+				log.Errorf("Read body err: %v", err)
+				return
 			}
+			log.Infof("%s", string(body))
 		}
 	})
 	_ = r.Run(config.Addr) // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
