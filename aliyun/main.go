0d20d9c736f654d2a9e4efa78ade66d62cc59c8d
zhangwei <sunnuslalala@gmail.com>
2020-03-21 01:19:04 +0800

    aliyun trigger

--- aliyun/main.go
+++ aliyun/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"github.com/arugal-docker/example-kube/logger"
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
@@ -16,12 +17,15 @@ func init() {
 func main() {
 	config := Config{Addr: ":8080"}
 	config.addFlags()
+	flag.Parse()
 
 	r := gin.Default()
 	r.POST("/payload", func(c *gin.Context) {
 		if c.Request.Body != nil {
+			defer c.Request.Body.Close()
 			var body []byte
 			_, err := c.Request.Body.Read(body)
+			log.Infof("%s", string(body))
 			if err == nil {
 				trigger := Trigger{}
 				err = json.Unmarshal(body, trigger)
@@ -30,7 +34,6 @@ func main() {
 				}
 			}
 		}
-		c.Status(200)
 	})
 	_ = r.Run(config.Addr) // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
